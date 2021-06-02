ebd98b10c2d77c946f06a83c975d537f93929a0e
Jyoshna9346 <69499416+Jyoshna9346@users.noreply.github.com>
2021-06-02 11:30:38 +0530

    Add files via upload

---
+++ numberspatterns.py
@@ -0,0 +1,24 @@
+'''
+num=int(input())
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        print(i+j,end="")
+    print()
+
+2
+234
+345
+456
+'''
+'''
+num=int(input())
+for i in range(1,num+1):
+    for j in range(1,num+1):
+        print(i,end="")
+    print()
+
+3
+111
+222
+333
+'''

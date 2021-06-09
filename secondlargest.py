9ccc9278f66f2d8bb055a67293b1de27da998699
Jyoshna9346 <69499416+Jyoshna9346@users.noreply.github.com>
2021-06-09 15:19:02 +0530

    Add files via upload

---
+++ secondlargest.py
@@ -0,0 +1,20 @@
+#secondlargest
+
+def secondlargest(n,data):
+    sh,fh=data[0],data[0]
+    for i in data[1:]:
+            if i>fh:
+                sh=fh
+                fh=i
+            if i>sh and i<fh:
+                sh=i
+    return sh
+n=int(input())
+data=list(map(int,input().split()))
+print(secondlargest(n,data))
+
+'''
+4
+1 2 7 8
+7
+'''

f1103b8fc57bd187f03654a64ec7ef59876c23f6
Jyoshna9346 <69499416+Jyoshna9346@users.noreply.github.com>
2021-06-15 11:25:19 +0530

    Add files via upload

---
+++ binary number with alternating bits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        d=n%2
+        n=n//2
+        while n:
+            r=n%2
+            n=n//2
+            if r==d:
+                return False
+            d=r
+        return True
+Your input
+5
+Output
+true
+Expected
+true

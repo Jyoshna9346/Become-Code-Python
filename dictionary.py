2ea5b31a4e13bfec72485375a9f901fb0c255d4d
Jyoshna9346 <69499416+Jyoshna9346@users.noreply.github.com>
2021-06-28 20:47:47 +0530

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -27,8 +27,8 @@ chandra
 
 '''
 
-'''
 #printing count of a numbers in a dic
+'''
 n=int(input())
 data=list(map(int,input().split()))
 dic={}
@@ -73,7 +73,173 @@ for k in dic.keys():
 3 4
 4 4
 '''
+'''
+s=input()
+n=len(s)
+print(s)
+
+for i in range(n):
+    print(s[i],end="")
+print()
+for i in s:
+    print(i,end="")
 
 
+this is python class
+this is python class
+this is python class
+this is python class
+'''
+#it prints alphabets values
+'''
+s=input()
+a=[0]*26
+for i in s:
+    print(ord(i))
 
+q
+113
+a
+97
+A
+65
+'''
+'''
+s=input()
+a=[0]*26
+A=[0]*26
+for i in s:
+    if i!=' ':
+        a[ord(i)-97]+=1
+print(a)
+
+jyoshna likes instagram
+[3, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 2, 1, 0, 0, 1, 3, 1, 0, 0, 0, 0, 1, 0]
+
+this is python class
+[1, 0, 1, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 1, 1, 1, 0, 0, 4, 2, 0, 0, 0, 0, 1, 0]
+
+'''
+#prints letters count in given sentences 
+'''
+s=input()
+dic={}
+for i in s:
+    if i not in dic:
+        dic[i]=1
+    else:
+        dic[i]+=1
+print(dic)
+
+this is python class
+{'t': 2, 'h': 2, 'i': 2, 's': 4, ' ': 3, 'p': 1, 'y': 1, 'o': 1, 'n': 1, 'c': 1, 'l': 1, 'a': 1}
+
+'''
+#it prints letters in ascending order
+'''
+s=input()
+a=[0]*26
+for i in s:
+    if i!=' ':
+        a[ord(i)-97]+=1
+for i in range(26):
+    if a[i]!=0:
+        print(chr(i+97))
+this is python class
+a
+c
+h
+i
+l
+n
+o
+p
+s
+t
+y
+
+'''
+'''
+s=input()
+data=s.split()
+print(len(data))
+
+this is python class
+4
+'''
+'''
+s=input()
+data=s.split()
+for i in data:
+    print(len(i))
+this is python class
+4
+2
+6
+5
+'''
+'''
+s=input()
+data=s.split()
+l=0
+pos=0
+for i in range(len(data)):
+    if len(data[i])>1:
+        l=len(data[i])
+        pos=i
+print(pos+1,data[pos],1)
+
+this is python class
+4 class 1
+'''
+#printing a sentences in reverse order
+'''
+s=input()
+s=s[::-1]
+print(s)
+
+this is python class
+ssalc nohtyp si siht
+'''
+#printing each word in sentence in reverse order
+'''
+def fun(s):
+    data=s.split()
+    s=' '
+    for w in data:
+        s=s+' '+w[::-1]
+    return s
+s=input()
+s=fun(s)
+print(s)
+
+
+this is python class
+siht si nohtyp ssalc
+
+'''
+#OR
+'''
+def fun(s):
+    data=s.split()
+    s=' '
+    for i in range(len(data)):
+        data[i]=data[i][::-1]
+    return ' '.join(data)
+s=input()
+s=fun(s)
+print(s)
+'''
+def fun(s):
+    data=s.split()
+    i=0
+    j=len(data)-1
+    while(i <j):
+        data[i],data[j]=data[j],data[i]
+        i+=1
+        j-+1
+    return ' '.join(data)
+s=input()
+s=fun(s)
+print(s)
 

1012b1c46531cb30bf5e660b868cb92689c7fbfd
Jyoshna9346 <69499416+Jyoshna9346@users.noreply.github.com>
2021-07-17 21:23:15 +0530

    Add files via upload

---
+++ binary representation.py
@@ -0,0 +1,11 @@
+
+def isprime(self,n):
+    if n==1:
+        return False
+    eq=int(math.sqrt(n))
+    for i in range(2,sq+1):
+        if n%i==0:
+            return False
+    return True
+def countprimesetbits(self,left:int,right:int)-->int:
+    prime(self.isprime(1))
